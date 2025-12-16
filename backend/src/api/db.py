3f4fe838a03b704091a5625166c211d644c3073d
Tommi Mäkelä <Tommi_makela@outlook.com>
2025-12-16 17:14:08 +0200

    Add updated database and API changes

---
+++ backend/src/api/db.py
@@ -0,0 +1,21 @@
+import os
+
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise NotImplementedError("DATABASE_URL environment variable is not set")
+
+
+engine = create_engine(DATABASE_URL)
+
+
+def init_db():
+    print("creating database tables...")
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
